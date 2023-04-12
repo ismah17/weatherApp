264c26970d2a2a3ca8c21b3765cce36739615c12
ismah17 <ishanimahanama91@gmail.com>
2023-04-12 20:30:11 +0200

    weather app to fetch API data and populate a website on command

---
+++ script.js
@@ -0,0 +1,28 @@
+let weather = {
+  apiKey: "123",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&APPID=" +
+        this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(icon, description, temp, humidity, speed);
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "wind speed:" + speed + "Km/h";
+    document.querySelector(".icon").src = "wind speed:" + speed + "Km/h";
+    //add icon here
+  },
+};
